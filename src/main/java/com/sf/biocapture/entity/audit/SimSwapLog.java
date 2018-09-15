503c7322684464fdad5ab1cffe9fcaff1c52d4ba
Marcel Ugwu <mugwu@seamfix.com>
2018-09-15 20:25:41 +0100

    Added capture mode and realtime device id to simswap log.

--- src/main/java/com/sf/biocapture/entity/audit/SimSwapLog.java
+++ src/main/java/com/sf/biocapture/entity/audit/SimSwapLog.java
@@ -1,231 +1,254 @@
 package com.sf.biocapture.entity.audit;
 
 import com.sf.biocapture.entity.EnrollmentRef;
-import java.sql.Timestamp;
+import com.sf.biocapture.bs.entity.enums.CaptureModeEnum;
+import com.sf.biocapture.entity.enums.SimSwapStatus;
+import nw.orm.core.IEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.Table;
-
-import com.sf.biocapture.entity.enums.SimSwapStatus;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-
-import nw.orm.core.IEntity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
 
 /**
- * 
  * @author Nnanna
  * @since 24/11/2016
- *
  */
 @Entity
 @Table(name = "SIM_SWAP_LOG")
 public class SimSwapLog extends IEntity {
-	private static final long serialVersionUID = 1860292452405267656L;
+    private static final long serialVersionUID = 1860292452405267656L;
 
-	/**
-	 * msisdn to be swapped
-	 */
-	@Column(name = "MSISDN", nullable = false)
-	private String msisdn;
-	
-	/**
-	 * Sim Serial of the old sim
-	 */
-	@Column(name = "OLD_SIM_SERIAL")
-	private String oldSimSerial;
-	
-	/**
-	 * IMSI. optional for now
-	 */
-	@Column(name = "ORDER_NUMBER")
-	private String orderNumber;
-	
-	/**
-	 * PUK of the new sim
-	 */
-	@Column(name = "PUK")
-	private String puk;
-	
-	/**
-	 * Sim Serial of the new sim
-	 */
-	@Column(name = "NEW_SIM_SERIAL", nullable = false)
-	private String simSerial;
-	
-	/**
-	 * kit tag
-	 */
-	@Column(name = "KIT_TAG", nullable = false)
-	private String tag;
-	
-	/**
-	 * kit mac address
-	 */
-	@Column(name = "MAC_ADDRESS", nullable = false)
-	private String macAddress;
-	
-	/**
-	 * unique ID
-	 */
-	@Column(name = "UNIQUE_ID", nullable = false)
-	private String uniqueId;
-	
-	/**
-	 * email address of the agent doing the swap
-	 */
-	@Column(name = "AGENT_EMAIL", nullable = false)
-	private String agentEmail;
-	
-	/**
-	 * time the swap was initiated
-	 */
-	@Column(name = "SWAP_TIME", nullable = false)
-	private Timestamp swapTime;
-	
-	/**
-	 * time the swap was confirmed 
-	 */
-	@Column(name = "CONFIRMATION_TIMESTAMP")
-	private Timestamp confirmationTimestamp;
-	
-	/**
-	 * status of the sim swap
-	 */
-	@Enumerated(EnumType.STRING)
-	@Column(name = "SWAP_STATUS", nullable = false)
-	private SimSwapStatus swapStatus;
-        
-	/**
-	 * portrait of the subscriber who carried out the swap
-	 */
-	@Lob
-	@Column(name = "SUBSCRIBER_PASSPORT", nullable = true)
-        private byte [] subscriberPassport;
-        
-        /**
-         * Enrollment Ref
-         */
-        @ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ENROLLMENT_REF_FK", nullable = true)
-        private EnrollmentRef enrollmentRef;
-        
-	public SimSwapLog(){}
+    /**
+     * msisdn to be swapped
+     */
+    @Column(name = "MSISDN", nullable = false)
+    private String msisdn;
 
-	public String getMsisdn() {
-		return msisdn;
-	}
+    /**
+     * Sim Serial of the old sim
+     */
+    @Column(name = "OLD_SIM_SERIAL")
+    private String oldSimSerial;
 
-	public void setMsisdn(String msisdn) {
-		this.msisdn = msisdn;
-	}
+    /**
+     * IMSI. optional for now
+     */
+    @Column(name = "ORDER_NUMBER")
+    private String orderNumber;
 
-	public String getOrderNumber() {
-		return orderNumber;
-	}
+    /**
+     * PUK of the new sim
+     */
+    @Column(name = "PUK")
+    private String puk;
 
-	public void setOrderNumber(String orderNumber) {
-		this.orderNumber = orderNumber;
-	}
+    /**
+     * Sim Serial of the new sim
+     */
+    @Column(name = "NEW_SIM_SERIAL", nullable = false)
+    private String simSerial;
 
-	public String getPuk() {
-		return puk;
-	}
+    /**
+     * kit tag
+     */
+    @Column(name = "KIT_TAG", nullable = false)
+    private String tag;
 
-	public void setPuk(String puk) {
-		this.puk = puk;
-	}
+    /**
+     * kit mac address
+     */
+    @Column(name = "MAC_ADDRESS", nullable = false)
+    private String macAddress;
 
-	public String getSimSerial() {
-		return simSerial;
-	}
+    /**
+     * unique ID
+     */
+    @Column(name = "UNIQUE_ID", nullable = false)
+    private String uniqueId;
 
-	public void setSimSerial(String simSerial) {
-		this.simSerial = simSerial;
-	}
+    /**
+     * email address of the agent doing the swap
+     */
+    @Column(name = "AGENT_EMAIL", nullable = false)
+    private String agentEmail;
 
-	public String getTag() {
-		return tag;
-	}
+    /**
+     * time the swap was initiated
+     */
+    @Column(name = "SWAP_TIME", nullable = false)
+    private Timestamp swapTime;
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    /**
+     * time the swap was confirmed
+     */
+    @Column(name = "CONFIRMATION_TIMESTAMP")
+    private Timestamp confirmationTimestamp;
 
-	public String getMacAddress() {
-		return macAddress;
-	}
+    /**
+     * status of the sim swap
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "SWAP_STATUS", nullable = false)
+    private SimSwapStatus swapStatus;
 
-	public void setMacAddress(String macAddress) {
-		this.macAddress = macAddress;
-	}
+    /**
+     * portrait of the subscriber who carried out the swap
+     */
+    @Lob
+    @Column(name = "SUBSCRIBER_PASSPORT", nullable = true)
+    private byte[] subscriberPassport;
 
-	public String getAgentEmail() {
-		return agentEmail;
-	}
+    /**
+     * Enrollment Ref
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ENROLLMENT_REF_FK", nullable = true)
+    private EnrollmentRef enrollmentRef;
 
-	public void setAgentEmail(String agentEmail) {
-		this.agentEmail = agentEmail;
-	}
+    @Column(name = "CAPTURE_MODE")
+    private String captureMode;
 
-	public Timestamp getConfirmationTimestamp() {
-		return confirmationTimestamp;
-	}
+    /**
+     * this is added to capture realtime device id for biosmart on web registration
+     */
+    @Column(name = "REALTIME_DEVICE_ID")
+    private String realtimeDeviceId;
 
-	public void setConfirmationTimestamp(Timestamp confirmationTimestamp) {
-		this.confirmationTimestamp = confirmationTimestamp;
-	}
+    public SimSwapLog() {
+    }
 
-	public SimSwapStatus getSwapStatus() {
-		return swapStatus;
-	}
+    public String getMsisdn() {
+        return msisdn;
+    }
 
-	public void setSwapStatus(SimSwapStatus swapStatus) {
-		this.swapStatus = swapStatus;
-	}
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
 
-	public String getOldSimSerial() {
-		return oldSimSerial;
-	}
+    public String getOrderNumber() {
+        return orderNumber;
+    }
 
-	public void setOldSimSerial(String oldSimSerial) {
-		this.oldSimSerial = oldSimSerial;
-	}
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
 
-	public Timestamp getSwapTime() {
-		return swapTime;
-	}
+    public String getPuk() {
+        return puk;
+    }
 
-	public void setSwapTime(Timestamp swapTime) {
-		this.swapTime = swapTime;
-	}
+    public void setPuk(String puk) {
+        this.puk = puk;
+    }
 
-        public byte[] getSubscriberPassport() {
-            return subscriberPassport;
-        }
+    public String getSimSerial() {
+        return simSerial;
+    }
 
-        public EnrollmentRef getEnrollmentRef() {
-            return enrollmentRef;
-        }
+    public void setSimSerial(String simSerial) {
+        this.simSerial = simSerial;
+    }
 
-        public void setRef(EnrollmentRef enrollmentRef) {
-            this.enrollmentRef = enrollmentRef;
-        }
+    public String getTag() {
+        return tag;
+    }
 
-        public void setSubscriberPassport(byte[] subscriberPassport) {
-            this.subscriberPassport = subscriberPassport;
-        }
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 
-        public String getUniqueId() {
-                return uniqueId;
-        }
+    public String getMacAddress() {
+        return macAddress;
+    }
 
-        public void setUniqueId(String uniqueId) {
-                this.uniqueId = uniqueId;
-        }        
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public String getAgentEmail() {
+        return agentEmail;
+    }
+
+    public void setAgentEmail(String agentEmail) {
+        this.agentEmail = agentEmail;
+    }
+
+    public Timestamp getConfirmationTimestamp() {
+        return confirmationTimestamp;
+    }
+
+    public void setConfirmationTimestamp(Timestamp confirmationTimestamp) {
+        this.confirmationTimestamp = confirmationTimestamp;
+    }
+
+    public SimSwapStatus getSwapStatus() {
+        return swapStatus;
+    }
+
+    public void setSwapStatus(SimSwapStatus swapStatus) {
+        this.swapStatus = swapStatus;
+    }
+
+    public String getOldSimSerial() {
+        return oldSimSerial;
+    }
+
+    public void setOldSimSerial(String oldSimSerial) {
+        this.oldSimSerial = oldSimSerial;
+    }
+
+    public Timestamp getSwapTime() {
+        return swapTime;
+    }
+
+    public void setSwapTime(Timestamp swapTime) {
+        this.swapTime = swapTime;
+    }
+
+    public byte[] getSubscriberPassport() {
+        return subscriberPassport;
+    }
+
+    public void setSubscriberPassport(byte[] subscriberPassport) {
+        this.subscriberPassport = subscriberPassport;
+    }
+
+    public EnrollmentRef getEnrollmentRef() {
+        return enrollmentRef;
+    }
+
+    public void setRef(EnrollmentRef enrollmentRef) {
+        this.enrollmentRef = enrollmentRef;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public CaptureModeEnum getCaptureMode() {
+        return this.captureMode == null ? null : CaptureModeEnum.valueOf(this.captureMode);
+    }
+
+    public void setCaptureMode(CaptureModeEnum captureModeEnum) {
+        this.captureMode = captureModeEnum == null ? null : captureModeEnum.name();
+    }
+
+    public String getRealtimeDeviceId() {
+        return realtimeDeviceId;
+    }
+
+    public void setRealtimeDeviceId(String realtimeDeviceId) {
+        this.realtimeDeviceId = realtimeDeviceId;
+    }
 }
